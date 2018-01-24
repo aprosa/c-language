841c71899c3922c806359486b8db9d1e35fdf854
aprosa <35294370+aprosa@users.noreply.github.com>
2018-01-24 16:01:42 +0530

    palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int check_palindrome(int num)
+{
+static int reverse_num=0,rem;
+if(num!=0)
+{
+rem=num%11;
+reverse_num=reverse_num*11+rem;
+check_palindrome(num/11);
+}
+return reverse_num;
+}
+int main()
+{
+int num, reverse_num;
+printf("Enter a number: ");
+scanf("%d",&num);
+reverse_num = check_palindrome(num);
+if(num==reverse_num)
+printf("%d is a palindrome number",num);
+else
+printf("%d is not a palindrome number",num);
+return 0;
+}
