e2448b26bf4557caaf807cc50cbed991eb6bc3af
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-05 16:36:16 +0530

    remainder.c

---
+++ remainder.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int number,original number,remainter,result=0;
+printf("enter a two digit integer");
+scanf("%d",&number);
+orginal number=number;
+while(original number=0)
+{
+remainder=orginal number;
+result=remainder*remainder;
+orginal number\=10;
+}
+if(result==number)
+printf("%d is a armstrong number");
+else
+printf("%d is not armstrong number");
+return 0;
+}
