6f2dfa1d4e1944fcba5133420b7e9cce177df578
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-07 16:16:16 +0530

    sums.c

---
+++ sums.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+ {
+int number, sums,i,array[10];
+scanf("%d", &number);
+for (i = 0; i < number; i++)
+{
+scanf("%d", &array);
+sums = sums + array[c];
+}
+printf("Sum = %d\n",sums);
+return 0;
+}
