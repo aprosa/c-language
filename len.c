ad4b2c6802cb9d532016ef1f79ed6e9abf59af66
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-07 16:20:35 +0530

    len.c

---
+++ len.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+char num[10];
+int numeric = 0, i, j, len;
+printf("Enter a number: ");
+len = strlen(num);
+for(i=0; i<len; i++){
+numeric = numeric * 10 + ( numeric[i] - '0' );
+}
+printf("%d", numeric);
+return 0;
+}
