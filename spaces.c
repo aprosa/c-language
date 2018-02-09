5cb417c603a7ce818508d7c3bf6ef22259149c9e
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-09 15:51:21 +0530

    space.c

---
+++ spaces.c
@@ -0,0 +1,20 @@
+include<stdio.h>
+int main()
+{
+int m,spaces;
+chara[20];
+printf("hello world");
+scanf("%s",a);
+for(m=0;a[10],i++)
+{
+if(a[i]="")
+{
+space++;
+}
+}
+printf("number of spaces");
+scanf("%d",space);
+printf("number of words");
+scanf("%d",words);
+return 0;
+}
