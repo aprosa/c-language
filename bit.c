af3096a55c41ccc24ee90da02a4039d8359a375b
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-12 16:07:11 +0530

    bit.c

---
+++ bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void swap(int*, int *);
+void main()
+{
+int n1,n2,temp1,temp2;
+printf("\nEnter two numbers");
+scanf("%d %d",&n1,&n2);
+printf("\nThe numbers before swapping are Num1= %d Num2);
+scanf("%d",n1,n2);
+swap(&num1, &num2);        
+printf("\nThe numbers after swapping are Num1= %d Num2);
+}
+void swap(int *a, int *b)
+{
+temp1=*a;
+*a=*b;
+*b=temp2;
+}
