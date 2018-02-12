fc305f0cf3efc1d5dd2f2f5552a641dff7625e85
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-12 15:54:54 +0530

    fibo.c

---
+++ fibo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,n,a=0,b=1,next terms");
+printf("\nenter the number of terms");
+scanf("%d",&n);
+printf("\nfibonacci series");
+for(i=1;i<=n;i++)
+{
+printf("%d",a);
+next term=a+b;
+a=b;
+b=next term;
+}
+return 0;
+}
