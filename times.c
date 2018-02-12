6309033a0e23cd0c0b3eeb7be7f77f2c1cedf7f6
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-12 16:37:11 +0530

    times.c

---
+++ times.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+int a,n;
+printf("\nHow many times you want to display hello world?);
+scanf("%d", &n);
+for (c=1; c<=n; c++)
+printf("\nHello world);
+return 0;
+}
