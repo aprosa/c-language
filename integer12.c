0130ef061ca2d6c49833756bf53a12f63ee31e7c
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-20 22:06:07 +0530

    integer12.c

---
+++ integer12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int count = 0;
+printf("\nEnter an integer");
+scanf("%lld", &n);
+while(n != 0)
+{
+n = n/10
+n /= 10;
+++count;
+}
+printf("\nNumber of digits");
+scanf("%d", count);
+return 0;
+}
