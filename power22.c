232f077439ed51d68512cc6dd55cfe0869b0b1db
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-21 16:34:01 +0530

    power22.c

---
+++ power22.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+int num;
+int temp,m,flag;
+printf("\nenter the integer number");
+scanf("%d",&num);
+temp=num;
+flags=0;
+while(tempnum%2=0)
+{
+if(tempn%2!=0)
+{
+flag=1;
+break;
+}
+tempnum=tempnum/2;
+if(flag==0)
+printf("%d",is a number that is power of z",num);
+else
+printf("%d is not the power of 2);
+return 0;
+}
+}
