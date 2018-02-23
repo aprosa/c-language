e3f67bb1190d7d995f4e0b492121da16b4cf2b61
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-23 16:37:31 +0530

    yes2.c

---
+++ yes2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+	int m;
+	scanf("%d",&m);
+    if(m>=1 && m<=10)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+	return 0;
+}
