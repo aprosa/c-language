064030d8da8f4e0535e5e5dbdec52fb7a4c7ab50
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-23 16:04:18 +0530

    sum13.c

---
+++ sum13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+float avg;
+printf("\nenter first value");
+scanf("%d",&a);
+printf("\nenter second value");
+scanf("%d",&b);
+sum=a+b;
+avg=(float)a+b/2;
+printf("\n sum of %d and %d is %d", a,b,sum);
+printf("\n average of %d and %d is %f",a,b.avg);
+return 0;
+}
