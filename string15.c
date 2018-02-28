07203ba680661ffce5f201f7cfab13ccb4ad6f57
aprosa <35294370+aprosa@users.noreply.github.com>
2018-02-28 16:55:08 +0530

    string15.c

---
+++ string15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int a[50],i;
+printf("\nenter the string");
+scanf("%d",&a);
+if(a[i]=='0'||'1')
+   {
+i++;
+printf("\nyes");
+   }
+else
+    {
+i++;
+printf("\nno");
+    }
+
