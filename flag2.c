a436cf7c8ecf64adf68fd402cb02b3520aebb54b
aprosa <35294370+aprosa@users.noreply.github.com>
2018-03-09 16:36:38 +0530

    flag2.c

---
+++ flag2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int num,i,flag;
+scanf("%d",&num);
+for(i=2;i<=num/2;i++)
+{
+if(num%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
