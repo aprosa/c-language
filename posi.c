754e33d27ed84256045bc6667351cd72fba8d459
aprosa <35294370+aprosa@users.noreply.github.com>
2018-03-19 16:05:38 +0530

    posi.c

---
+++ posi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a[10],n,m,i;
+printf("Enter the number of integers:")
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("Enter the position:")
+scanf("%d",&m);
+printf("%d",a[m-1]);
+}
